5b53d64c8b9501cff536ed7b4713ea1b925d8715
ownport <ownport@gmail.com>
2014-03-28 00:14:08 +0200

    add simple file handling (delete)

--- aria2clib/simple.py
+++ aria2clib/simple.py
@@ -17,6 +17,8 @@
 # - [JSON-RPC 2.0 Specification](http://www.jsonrpc.org/specification)
 #
 
+from hashlib import md5
+
 from legacy import LegacyClient
 
 
@@ -26,8 +28,14 @@ class SimpleClient(object):
     def __init__(self, client_id=None, uri=None, username=None, password=None):
         ''' __init__
         '''
-        self._client = Aria2cJsonClient()
+        self._client = Aria2cJsonClient(client_id=client_id, uri=uri, username=username, password=password)
     
+    
+    def _make_gid(self, url):
+        ''' return gid as hash of url
+        '''
+        return md5(url).hexdigest()
+        
     
     def get(self, gid=[]):
         ''' get status of files
@@ -35,21 +43,47 @@ class SimpleClient(object):
         return []
     
     
-    def put(self, url, params={}):
+    def put(self, url, params=[], pause=False):
         ''' put url(s) to queue for downloading
+        
+        if pause=True, If the download is active, the download is placed on the first position of waiting queue. 
+        As long as the status is paused, the download is not started. To continue download, call put() with pause=False
+        for those urls
         '''
-        return []
+        
+        result = self._client.add_uri(url, params)        
+
+        return result
     
     
-    def delete(self, gid):
-        ''' delete file(s) by gid(s)
+    def delete(self, url, force=False):
+        ''' delete file(s) by url(s)
+        
+        - gid can be as single value or a list of gids
+        
+        - force=True,  this method removes download without any action which takes time such as contacting 
+        BitTorrent tracker
+        - force=Falce (default), it is stopped download at first than download becomes removed.
         '''
-        return []
+        urls = list()
+       
+        # handle list or single url(s)       
+        if isinstance(url, (string, unicode, int)):
+            urls.append(url)
+        else:
+            urls.extend(url)
+        
+        for url in urls:
+            if force:
+                print self._client.force_remove(url)
+                continue
+            print self._client.remove(url)
 
 
     def stats(self):
         ''' return aria2c stats
         '''
+        
         return []            
 
         
