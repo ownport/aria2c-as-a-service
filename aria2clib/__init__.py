53e7993a8f2d630deb30c8e7170c6713bb65b44d
ownport <ownport@gmail.com>
2014-04-04 07:55:36 +0300

    move main components to be visible via aria2clib

--- aria2clib/__init__.py
+++ aria2clib/__init__.py
@@ -1,7 +1,7 @@
-from simple import SimpleClient
-from legacy import LegacyClient
-from legacy import GLOBAL_OPTION_FIELDS
-from legacy import GLOBAL_STATS_FIELDS
+from simple_api import SimpleClient
+from legacy_api import LegacyClient
+from legacy_api import GLOBAL_OPTION_FIELDS
+from legacy_api import GLOBAL_STATS_FIELDS
 
 __all__ = [
     'GLOBAL_OPTION_FIELDS', 'GLOBAL_STATS_FIELDS',
